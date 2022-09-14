ff60d58aff20ec4d6f18d674a9f411f63740786e
duchuy160520 <duchuy160520@gmail.com>
2022-09-14 21:48:34 +0700

    'controller'

---
+++ src/main/java/com/controllers/AccountController.java
@@ -0,0 +1,53 @@
+package datn.store.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import datn.store.entity.Accounts;
+import datn.store.service.AccountService;
+
+@Controller
+public class AccountController {
+	@Autowired
+	private AccountService accSer;
+
+	@RequestMapping("/accounts/index")
+	public String index(Model model) {
+		List<Accounts> acc = accSer.findAll();
+		model.addAttribute("acc", acc);
+		return "admin/accounts/index";
+	}
+
+	@RequestMapping(value = "/accounts/add")
+	public String create(Model model) {
+		model.addAttribute("accounts", new Accounts());
+		return "admin/accounts/add";
+	}
+
+	@RequestMapping(value = "/accounts/edit", method = RequestMethod.GET)
+	public String edit(@RequestParam("id") Integer id, Model model) {
+		Optional<Accounts> accA = accSer.findById(id);
+		accA.ifPresent(acc -> model.addAttribute("acc", acc));
+		return "admin/accounts/edit";
+	}
+
+	@RequestMapping(value = "/accounts/save", method = RequestMethod.POST)
+	public String save(Accounts acc) {
+		accSer.save(acc);
+		return "redirect:/accounts/index";
+	}
+
+	@RequestMapping(value = "/accounts/delete", method = RequestMethod.GET)
+	public String delete(@RequestParam("id") Integer id, Model model) {
+		accSer.delete(id);
+		return "redirect:/accounts/index";
+	}
+
+}
---
+++ src/main/java/com/controllers/CategoryController.java
@@ -0,0 +1,52 @@
+package datn.store.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import datn.store.entity.Category;
+import datn.store.service.CategoryService;
+
+@Controller
+public class CategoryController {
+	@Autowired
+	private CategoryService cateService;
+
+	@RequestMapping("/categorys/index")
+	public String index(Model model) {
+		List<Category> cates = cateService.getAlCategories();
+		model.addAttribute("cates", cates);
+		return "admin/category/index";
+	}
+
+	@RequestMapping(value = "/categorys/add")
+	public String addCate(Model model) {
+		model.addAttribute("category", new Category());
+		return "admin/category/addCate";
+	}
+
+	@RequestMapping(value = "/categorys/edit", method = RequestMethod.GET)
+	public String editCate(@RequestParam("id") Long cateId, Model model) {
+		Optional<Category> cateEdit = cateService.findCateById(cateId);
+		cateEdit.ifPresent(cate -> model.addAttribute("cate", cate));
+		return "admin/category/editCate";
+	}
+
+	@RequestMapping(value = "/categorys/save", method = RequestMethod.POST)
+	public String save(Category cate) {
+		cateService.saveCate(cate);
+		return "redirect:/categorys/index";
+	}
+
+	@RequestMapping(value = "/categorys/delete", method = RequestMethod.GET)
+	public String deleteCate(@RequestParam("id") Long cateId, Model model) {
+		cateService.deleteCate(cateId);
+		return "redirect:/categorys/index";
+	}
+}
